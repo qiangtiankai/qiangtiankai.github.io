32e5b0341192b1421cb2046c5d7ce6fac8303f7c
Qiang Tiankai <987866823@qq.com>
2026-08-24 02:38:28 +0800

    feat: J盘海报拼接背景随滚动; 社区去暖; 移除图片金边框; 金转银光泽; Hero名字缩小留白

--- app.jsx
+++ app.jsx
@@ -28,18 +28,24 @@ function App() {
   }
 
   return (
-    <div>
-      {/* 背景：暗调展览海报拼接 + 玻璃高光 */}
+    <div className="app-root">
+      {/* 背景：暗调展览海报拼接（J 盘素材）+ 玻璃高光，随页面滚动 */}
       <div className="ambient-collage" aria-hidden="true">
-        <div className="ambient-tile" style={{ backgroundImage: 'url(https://aka.doubaocdn.com/s/gWxpkjNgX5)' }} />
-        <div className="ambient-tile" style={{ backgroundImage: 'url(https://aka.doubaocdn.com/s/xmuyGNZVho)' }} />
-        <div className="ambient-tile" style={{ backgroundImage: 'url(https://aka.doubaocdn.com/s/jtNqCh8idx)' }} />
-        <div className="ambient-tile" style={{ backgroundImage: 'url(https://aka.doubaocdn.com/s/LkuDSjeOFm)' }} />
-        <div className="ambient-tile" style={{ backgroundImage: 'url(https://aka.doubaocdn.com/s/AfH5NefCRF)' }} />
-        <div className="ambient-tile" style={{ backgroundImage: 'url(https://aka.doubaocdn.com/s/C3g0BHTjqp)' }} />
-        <div className="ambient-tile" style={{ backgroundImage: 'url(https://aka.doubaocdn.com/s/df3GiCQH0W)' }} />
-        <div className="ambient-tile" style={{ backgroundImage: 'url(https://aka.doubaocdn.com/s/a64X8A5HDK)' }} />
-        <div className="ambient-tile" style={{ backgroundImage: 'url(https://aka.doubaocdn.com/s/bMc6BD8kMk)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/海报 (1).jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/海报 (2).jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/海报 (3).jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/海报 (4).jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/海报 (5).jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/海报 (6).jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/海报 (7).jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/海报 (8).jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/海报 (9).jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/海报 (10).jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/海报 (11).jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/海报 (12).jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/终版海报.jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/梦江水海报.jpg)' }} />
+        <div className="ambient-tile" style={{ backgroundImage: 'url(posters/喃喃海报.png)' }} />
         <div className="ambient-scrim" />
         <div className="ambient-grain" />
       </div>
